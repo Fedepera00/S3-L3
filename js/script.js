0a64b2f23c84b1e1496ebfb8cada9856fddeb4a7
Federico Peralta <federico.peralta91@gmail.com>
2024-08-28 16:43:58 +0200

    S3-L3 progetto

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.getElementById("addTaskButton").addEventListener("click", function () {
+  const taskText = document.getElementById("taskInput").value;
+
+  if (taskText === "") {
+    alert("Per continuare inserisci un nuovo elemento  ");
+    return;
+  }
+
+  const taskList = document.getElementById("taskList");
+
+  const listItem = document.createElement("li");
+  listItem.className = "task-item";
+
+  const taskSpan = document.createElement("span");
+  taskSpan.textContent = taskText;
+  taskSpan.addEventListener("click", function () {
+    this.classList.toggle("completato");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Elimina";
+  deleteButton.className = "delete-btn";
+  deleteButton.addEventListener("click", function () {
+    taskList.removeChild(listItem);
+  });
+
+  listItem.appendChild(taskSpan);
+  listItem.appendChild(deleteButton);
+  taskList.appendChild(listItem);
+
+  document.getElementById("taskInput").value = "";
+});
